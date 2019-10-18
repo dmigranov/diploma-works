f7c186e83d1b4837a8ad4ab7b26108b8feddaa80
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-18 20:58:24 +0700

    maven added apache libreay

--- Wireframe/src/main/java/ru/nsu/fit/g16201/migranov/model/GeodesicsCalculator.java
+++ Wireframe/src/main/java/ru/nsu/fit/g16201/migranov/model/GeodesicsCalculator.java
@@ -1,4 +1,11 @@
 package ru.nsu.fit.g16201.migranov.model;
 
+import org.apache.commons.math3.analysis.differentiation.DerivativeStructure;
+
 public class GeodesicsCalculator {
+    private Matrix calculateMetricTensor(double u0, double v0)
+    {
+        return new Matrix(1, 1, 1);
+    }
+
 }
