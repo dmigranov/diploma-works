5d4f8dc7070688efb9ce5ab8f2e254a033ee9794
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-27 18:04:16 +0700

    computeDerivatives done?

--- Wireframe/src/main/java/ru/nsu/fit/g16201/migranov/model/GeodesicsEquations.java
+++ Wireframe/src/main/java/ru/nsu/fit/g16201/migranov/model/GeodesicsEquations.java
@@ -22,10 +22,14 @@ public class GeodesicsEquations implements FirstOrderDifferentialEquations {
     public void computeDerivatives(double t, double[] y, double[] yDot) throws MaxCountExceededException, DimensionMismatchException {
         //0 - dx1', 1 - dx2', 2 - dx1, 3 - dx2
 
-        double sum = 0;
-        for(int k = 0; k < 2; k++)
-            for(int i = 0; i < 2; i++)
-                sum += -Cs[0][k][i] *
+        for(int j = 0; j < 2; j++)
+        {
+            double sum = 0;
+            for (int k = 0; k < 2; k++)
+                for (int i = 0; i < 2; i++)
+                    sum += -Cs[0][k][i] * y[k] * y[i];
+            yDot[j] = sum;
+        }
         yDot[2] = y[0];
         yDot[3] = y[1];
     }
