e0bec3e64e97b1e78dbf8d9095466637cc6d9094
Denis Migranov <d.migranov@g.nsu.ru>
2019-10-27 17:38:57 +0700

    перешел к FirstOrderDifferentialEquations, т.к можно перейти к системе первого порядка

--- Wireframe/src/main/java/ru/nsu/fit/g16201/migranov/model/GeodesicsEquations.java
+++ Wireframe/src/main/java/ru/nsu/fit/g16201/migranov/model/GeodesicsEquations.java
@@ -1,15 +1,18 @@
 package ru.nsu.fit.g16201.migranov.model;
 
-import org.apache.commons.math3.ode.SecondOrderDifferentialEquations;
+import org.apache.commons.math3.exception.DimensionMismatchException;
+import org.apache.commons.math3.exception.MaxCountExceededException;
+import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
 
-public class GeodesicsEquations implements SecondOrderDifferentialEquations {
+public class GeodesicsEquations implements FirstOrderDifferentialEquations {
+
     @Override
     public int getDimension() {
-        return 2;
+        return 4;
     }
 
     @Override
-    public void computeSecondDerivatives(double t, double[] y, double[] yDot, double[] yDDot) {
+    public void computeDerivatives(double t, double[] y, double[] yDot) throws MaxCountExceededException, DimensionMismatchException {
 
     }
 }
