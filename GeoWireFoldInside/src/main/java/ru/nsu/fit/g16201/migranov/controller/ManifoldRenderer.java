523db7778024ca999925c0c51fa6434988af1f96
Denis Migranov <d.migranov@g.nsu.ru>
2019-11-03 19:46:50 +0700

    saadsdd

--- GeoWireFoldInside/src/main/java/ru/nsu/fit/g16201/migranov/controller/ManifoldRenderer.java
+++ GeoWireFoldInside/src/main/java/ru/nsu/fit/g16201/migranov/controller/ManifoldRenderer.java
@@ -13,6 +13,7 @@ public class ManifoldRenderer {
     private ThreadPoolExecutor executor;
 
     private int[][] floatColors;
+    private double zn, sw, sh;
 
 
     public ManifoldRenderer(ManifoldInsidePanel panel)
@@ -20,19 +21,47 @@ public class ManifoldRenderer {
         this.panel = panel;
     }
 
-    public void render(int numberOfThreads)
+    public void render(int numberOfThreads, double zn, double sw, double sh)
     {
+        this.zn = zn;
+        this.sw = sw;
+        this.sh = sh;
         int width = panel.getWidth();
         int height = panel.getHeight();
 
         floatColors = new int[height][width];
 
-
         executor = new ThreadPoolExecutor(numberOfThreads, numberOfThreads, 1000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(width));
 
+        double nearStartX = - sw/2;
+        double nearStartY = - sh/2;
+
+        double dx = sw/width, dy = sh/height;
+        double x;
+        double y = nearStartY + dy/2;
+
+
+        x = nearStartX + dx/2;
+        for(int j = 0; j < width; j++)
+        {
+            //центры пикселя
+            executor.execute(new RendererTask(x, j));
+            x += dx;
+        }
+        y += dy;
+
+        executor.shutdown();
     }
 
-    public class RendererTask implements Runnable {
+    static class RendererTask implements Runnable {
+
+        private double pixelX;
+        private int picX;
+
+        RendererTask(double x, int j) {
+            pixelX = x;
+            picX = j;
+        }
 
         @Override
         public void run() {
