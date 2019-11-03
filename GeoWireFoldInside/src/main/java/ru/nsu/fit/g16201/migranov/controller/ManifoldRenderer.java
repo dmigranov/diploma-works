1a067af6a3a8f18361759158093d220d5ce08f4c
Denis Migranov <d.migranov@g.nsu.ru>
2019-11-03 19:38:11 +0700

    more renderer

--- GeoWireFoldInside/src/main/java/ru/nsu/fit/g16201/migranov/controller/ManifoldRenderer.java
+++ GeoWireFoldInside/src/main/java/ru/nsu/fit/g16201/migranov/controller/ManifoldRenderer.java
@@ -8,22 +8,37 @@ import java.util.concurrent.TimeUnit;
 
 public class ManifoldRenderer {
     private final double observerHeight = 1;
+
     private ManifoldInsidePanel panel;
     private ThreadPoolExecutor executor;
+
+    private int[][] floatColors;
 
 
     public ManifoldRenderer(ManifoldInsidePanel panel)
     {
-
         this.panel = panel;
     }
 
     public void render(int numberOfThreads)
     {
         int width = panel.getWidth();
+        int height = panel.getHeight();
+
+        floatColors = new int[height][width];
 
 
         executor = new ThreadPoolExecutor(numberOfThreads, numberOfThreads, 1000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(width));
 
     }
+
+    public class RendererTask implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+
 }
