fecb8c392cd836f5f6d866997c3b36d4de254d4b
Denis Migranov <d.migranov@g.nsu.ru>
2019-11-08 20:33:10 +0700

    dfsdf

---
+++ GeoWireFoldInside/src/main/java/ru/nsu/fit/g16201/migranov/model/SplineFunction.java
@@ -0,0 +1,22 @@
+package ru.nsu.fit.g16201.migranov.model;
+
+import java.util.function.Function;
+
+public class SplineFunction implements Function<double[], double[]> {
+
+    private SplineCalculator splineCalculator;
+
+    public SplineFunction(SplineCalculator splineCalculator)
+    {
+        this.splineCalculator = splineCalculator;
+    }
+
+
+    @Override
+    public double[] apply(double[] values) {
+        double u = values[0], v = values[1];
+        Point3D p = splineCalculator.calculateSplineFunction(u, v);
+        if(p == null) return null;
+        return new double[]{p.x, p.y, p.z};
+    }
+}
