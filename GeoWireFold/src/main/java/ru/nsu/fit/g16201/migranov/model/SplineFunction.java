995ae98e147aca68c945ff380b28006ad1e3db7d
Denis Migranov <d.migranov@g.nsu.ru>
2019-11-08 20:36:53 +0700

    gonna do some OOP

---
+++ GeoWireFold/src/main/java/ru/nsu/fit/g16201/migranov/model/SplineFunction.java
@@ -0,0 +1,20 @@
+package ru.nsu.fit.g16201.migranov.model;
+
+public class SplineFunction implements ManifoldFunction {
+
+    private SplineCalculator splineCalculator;
+
+    public SplineFunction(SplineCalculator splineCalculator)
+    {
+        this.splineCalculator = splineCalculator;
+    }
+
+
+    @Override
+    public double[] apply(double[] values) {
+        double u = values[0], v = values[1];
+        Point3D p = splineCalculator.calculateSplineFunction(u, v);
+        if(p == null) return null;
+        return new double[]{p.x, p.y, p.z};
+    }
+}
