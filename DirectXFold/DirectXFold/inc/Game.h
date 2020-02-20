147e0ad756806f796df1b1d44de59158e19275a6
Denis Migranov <d.migranov@g.nsu.ru>
2020-02-20 18:27:21 +0700

    чёто

--- DirectXFold/DirectXFold/inc/Game.h
+++ DirectXFold/DirectXFold/inc/Game.h
@@ -1,5 +1,15 @@
 class Game
 {
+public:
+	Game() noexcept;
 
 
+    // Initialization and management
+    void Initialize(HWND window, int width, int height);
+
+    // Basic game loop
+    void Tick();
+
+    // Properties
+    void GetDefaultSize(int& width, int& height);
 };
--- DirectXFold/DirectXFold/src/Game.cpp
+++ DirectXFold/DirectXFold/src/Game.cpp
@@ -1,2 +1,19 @@
 #include <pch.h>
 #include "Game.h"
+#include "..\inc\Game.h"
+
+Game::Game() noexcept
+{
+}
+
+void Game::Initialize(HWND window, int width, int height)
+{
+}
+
+void Game::Tick()
+{
+}
+
+void Game::GetDefaultSize(int& width, int& height)
+{
+}
