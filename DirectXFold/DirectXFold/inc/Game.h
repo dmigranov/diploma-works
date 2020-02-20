0d5d03b1b1851e4b52ee64a5c4d2a059eaf66a65
Denis Migranov <d.migranov@g.nsu.ru>
2020-02-20 19:17:19 +0700

    чёто

--- DirectXFold/DirectXFold/inc/Game.h
+++ DirectXFold/DirectXFold/inc/Game.h
@@ -29,6 +29,34 @@ private:
     int                                             m_outputWidth;
     int                                             m_outputHeight;
 
+    // Direct3D device and swap chain.
+    ID3D11Device* g_d3dDevice = nullptr;                                    //used for allocating GPU resources such as buffers, textures, shaders, and state objects
+    ID3D11DeviceContext* g_d3dDeviceContext = nullptr;                      //used to configure the rendering pipeline and draw geometry
+    IDXGISwapChain* g_d3dSwapChain = nullptr;                               //stores the buffers that are used for rendering data; used to determine how the buffers are swapped when the rendered image should be presented to the scree
+
+    // Render target view for the back buffer of the swap chain.
+    ID3D11RenderTargetView* g_d3dRenderTargetView = nullptr;
+    // Depth/stencil view for use as a depth buffer.
+    ID3D11DepthStencilView* g_d3dDepthStencilView = nullptr;
+    // A texture to associate to the depth stencil view.
+    ID3D11Texture2D* g_d3dDepthStencilBuffer = nullptr;
+
+    // Define the functionality of the depth/stencil stages.
+    ID3D11DepthStencilState* g_d3dDepthStencilState = nullptr;
+    // Define the functionality of the rasterizer stage.
+    ID3D11RasterizerState* g_d3dRasterizerState = nullptr;
+    D3D11_VIEWPORT g_Viewport = { 0 };                                      //The g_Viewport variable defines the size of the viewport rectangle. The viewport rectangle is also used by the rasterizer stage to determine the renderable area on screen.
+
+
+    // Vertex buffer data
+    ID3D11InputLayout * g_d3dInputLayout = nullptr;                         //used to describe the order and type of data that is expected by the vertex shader.
+    ID3D11Buffer* g_d3dVertexBuffer = nullptr;
+    ID3D11Buffer* g_d3dIndexBuffer = nullptr;
+
+    // Shader data
+    ID3D11VertexShader* g_d3dVertexShader = nullptr;
+    ID3D11PixelShader* g_d3dPixelShader = nullptr;
+
     bool                                            m_minimized = false;
 
 };
