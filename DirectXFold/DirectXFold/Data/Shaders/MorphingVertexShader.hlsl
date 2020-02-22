bacdcfaf0723e90b12863fe30242911e53bcde4e
Denis Migranov <d.migranov@g.nsu.ru>
2020-02-22 20:11:33 +0700

    шейдерим потихоньку

---
+++ DirectXFold/DirectXFold/Data/Shaders/MorphingVertexShader.hlsl
@@ -0,0 +1,44 @@
+cbuffer PerApplication : register(b0)
+{
+	matrix projectionMatrix;
+}
+
+cbuffer PerFrame : register(b1)
+{
+	matrix viewMatrix;
+}
+
+cbuffer PerObject : register(b2)
+{
+	matrix worldMatrix;
+}
+
+//POSITION and COLOR are semantics that are used to link vs variables to ps variables
+struct AppData
+{
+	float4 position : POSITION;
+	float3 color : COLOR;
+};
+
+struct VertexShaderOutput
+{
+	float4 color : COLOR;
+	float4 position : SV_POSITION; //system-value semantic that is required by the rasterizer stage
+};
+
+//entry point
+VertexShaderOutput SimpleVertexShader(AppData IN)
+{
+	VertexShaderOutput OUT;
+    
+	float3 p1 = IN.position;
+	float alpha = (p1.y + 1.0) / 2.0;
+    
+	matrix newWorld = worldMatrix;
+	newWorld[0][0] *= 2;
+	matrix mvp = mul(projectionMatrix, mul(viewMatrix, newWorld));
+	OUT.position = mul(mvp, IN.position);
+	OUT.color = float4(IN.color, 1.0f);
+ 
+	return OUT;
+}
