584842f18f02c65593fe0e36ff9598a2396b6d98
Denis Migranov <d.migranov@g.nsu.ru>
2020-03-04 15:27:33 +0700

    quo vadis

--- DirectXFold/DirectXFold/SphericalMath.cpp
+++ DirectXFold/DirectXFold/SphericalMath.cpp
@@ -3,6 +3,7 @@
 
 
 //z0 - radians
+//gives 90 degree FOV
 Matrix BananaProjectionMatrix(double z0)
 {
 	return Matrix(1.f, 0.f, 0.f, 0.f,
--- DirectXFold/DirectXFold/inc/SphericalMath.h
+++ DirectXFold/DirectXFold/inc/SphericalMath.h
@@ -2,6 +2,9 @@ using namespace DirectX::SimpleMath;
 using namespace DirectX;
 
 Matrix BananaProjectionMatrix(double z0);
+Matrix BananaProjectionMatrixFrontHalf(double z0);
+Matrix BananaProjectionMatrixBackHalf(double z0);
+
 
 Matrix SphericalRotationZW(double d);
 Matrix SphericalRotationXW(double d);
