fbb37eac8385d4f78dcd1f78d1391c5ae15055b7
Denis Migranov <d.migranov@g.nsu.ru>
2020-03-11 15:57:03 +0700

    w

--- DirectXFold/DirectXFold/inc/TextDrawer.h
+++ DirectXFold/DirectXFold/inc/TextDrawer.h
@@ -6,6 +6,7 @@ class TextDrawer
 {
 public:
 	TextDrawer(ID3D11Device* device, ID3D11DeviceContext* context, const wchar_t* fontFile);
+	void DrawText();
 private:
 	std::unique_ptr<DirectX::SpriteBatch> m_spriteBatch;
 	std::unique_ptr<DirectX::SpriteFont> m_font;
--- DirectXFold/DirectXFold/src/TextDrawer.cpp
+++ DirectXFold/DirectXFold/src/TextDrawer.cpp
@@ -1,9 +1,25 @@
 #include "pch.h"
 #include "TextDrawer.h"
 using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
 
 TextDrawer::TextDrawer(ID3D11Device* device, ID3D11DeviceContext *context, const wchar_t *fontFile)
 {
 	m_spriteBatch = std::make_unique<SpriteBatch>(context);
 	m_font = std::make_unique<SpriteFont>(device, fontFile);
+}
+
+void TextDrawer::DrawText()
+{
+	m_spriteBatch->Begin();
+
+	const wchar_t* output = L"Hello World";
+
+	Vector2 origin = m_font->MeasureString(output) / 2.f;
+
+	m_font->DrawString(m_spriteBatch.get(), output,
+		m_fontPos, Colors::White, 0.f, origin);
+
+	m_spriteBatch->End();
 }
