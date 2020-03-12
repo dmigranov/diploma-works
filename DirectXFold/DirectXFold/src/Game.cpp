b6d61211fd708fd2544050ba930bda9e52ee41dc
Denis Migranov <d.migranov@g.nsu.ru>
2020-03-12 14:37:08 +0700

    тетраэдр

--- DirectXFold/DirectXFold/src/Game.cpp
+++ DirectXFold/DirectXFold/src/Game.cpp
@@ -406,11 +406,7 @@ bool Game::LoadContent()
     g_d3dDeviceContext->UpdateSubresource(g_d3dConstantBuffers[CB_Application], 0, nullptr, &m_proj, 0, 0);*/
 
     {
-        Mesh::VertexPosColor vertices[] = {
-            { XMFLOAT4(0.6f, 0.0f, 0.0f, 0.8f), XMFLOAT3(0.0f, 0.0f, 0.0f) }, // 0
-            { XMFLOAT4(0.0f,  0.6f, 0.0f, 0.8f), XMFLOAT3(0.0f, 1.0f, 0.0f) }, // 1
-            { XMFLOAT4(0.0f,  -0.6f, 0.0f, 0.8f), XMFLOAT3(1.0f, 1.0f, 0.0f) }, // 2
-        };
+
 
         /*Mesh::VertexPosColor vertices[] = {
             { GetCartesianFromSpherical(1.37f, 2.17f, 1.f), XMFLOAT3(0.0f, 0.0f, 0.0f) }, // 0
@@ -424,17 +420,24 @@ bool Game::LoadContent()
             { GetCartesianFromSpherical(2.5f, XM_PIDIV2, XM_PIDIV4), XMFLOAT3(1.0f, 1.0f, 0.0f) }, // 2
         };*/
 
+        Mesh::VertexPosColor vertices[] = {
+            { XMFLOAT4(0.6f, 0.0f, 0.0f, 0.8f), XMFLOAT3(0.0f, 0.0f, 0.0f) },   // 0
+            { XMFLOAT4(0.0f,  0.6f, 0.0f, 0.8f), XMFLOAT3(0.0f, 1.0f, 0.0f) },  // 1
+            { XMFLOAT4(0.0f,  -0.6f, 0.0f, 0.8f), XMFLOAT3(1.0f, 1.0f, 0.0f) }, // 2
+            { XMFLOAT4(-0.33166f, 0.f, -0.5f, 0.8f), XMFLOAT3(1.0f, 0.0f, 0.0f) }   // 3
+        };
+
         WORD indices[] = {
-            0, 1, 2, 2, 1, 0
+            /*0, 1, 2*/ 2, 1, 0, 
+            1, 2, 3,
+            0, 1, 3,
+            3, 2, 0
         };
 
         mesh1 = new Mesh(_countof(vertices), vertices,
             _countof(indices), indices);
         meshes.push_back(mesh1);
 
-        Mesh * mesh = mesh1->Clone();
-        mesh->SetWorldMatrix(SphericalRotationXZ(0.6) * mesh->GetWorldMatrix());
-        meshes.push_back(mesh);
 
     }
 
