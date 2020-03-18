c8ad400e171d263c1be257da4216c545742403db
Denis Migranov <d.migranov@g.nsu.ru>
2020-03-18 14:43:43 +0700

    добавлен новый конструктор

--- DirectXFold/DirectXFold/inc/Octahedron.h
+++ DirectXFold/DirectXFold/inc/Octahedron.h
@@ -4,8 +4,16 @@ class Octahedron :
 	public Mesh
 {
 public:
+	enum FixedCoordinate {
+		FC_X,
+		FC_Y,
+		FC_Z,
+		FC_W
+	};
 	Octahedron(float wSec, XMMATRIX world);
 	Octahedron(float wSec);
+	Octahedron(FixedCoordinate coord, float section);
+
 
 };
 
