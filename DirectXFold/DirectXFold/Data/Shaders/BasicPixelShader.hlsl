6bfb7f6d61a3eec1dba5dcb7e910079566b24e65
Denis Migranov <d.migranov@g.nsu.ru>
2020-03-19 21:31:02 +0700

    покрасивше но бесмысленно

--- DirectXFold/DirectXFold/Data/Shaders/BasicPixelShader.hlsl
+++ DirectXFold/DirectXFold/Data/Shaders/BasicPixelShader.hlsl
@@ -12,7 +12,14 @@ float4 SimplePixelShader(PixelShaderInput IN) : SV_TARGET
 	float3 edgeDistance = IN.edgeDistance;
 	float distance = min(edgeDistance.x, min(edgeDistance.y, edgeDistance.z));
 	
-	if (distance < thickness)
+	if (distance == edgeDistance.x)
+		return float4(0.52, 0, 0, 1);
+	else if (distance == edgeDistance.y)
+		return float4(0, 0.52, 0, 1);
+	else// if (distance==GEdgeDistance[2])
+		return float4(0, 0, 0.52, 1);
+	
+	/*if (distance < thickness)
 		return float4(0, 0, 0, 1); //draw fragment if close to edge
-	return IN.color; //else; а можно дискард
+	return IN.color; //else; а можно дискард*/
 }
