11eeba865525fb9ae128a31258c6e8e28a6e1b18
Denis Migranov <d.migranov@g.nsu.ru>
2020-03-19 19:19:13 +0700

    ашипка + функция добавлена (без аргов пока)

--- DirectXFold/DirectXFold/GeometryShader.hlsl
+++ DirectXFold/DirectXFold/GeometryShader.hlsl
@@ -11,34 +11,46 @@ struct VertexOutput
     float4 position : SV_Position; //стандартный System-Value для вертекса, который требуется на RS
 };
 
+
+float3 GetCylinderPoint()
+{
+	return float3(0, 0, 0);
+
+}
+
 [maxvertexcount(3)] // максимальное кол-во вертексов, которое мы можем добавить
-void SimpleGeometryShader(triangle VertexInput input[3], inout TriangleStream<VertexOutput> stream)
+
+	void SimpleGeometryShader
+	(triangle
+	VertexInput input[3], inout
+	TriangleStream<VertexOutput> stream)
 {    
-    VertexOutput v1 = { input[0].color, {0, 0}, input[0].position };
-    stream.Append(v1); // добавление вертекса
-    VertexOutput v2 = { input[1].color, { 0, 0 }, input[1].position };
-    stream.Append(v2); // добавление вертекса
-    VertexOutput v3 = { input[2].color, { 0, 0 }, input[2].position };
-    stream.Append(v3); // добавление вертекса
+		VertexOutput v1 = { input[0].color, { 0, 0 }, input[0].position };
+		stream.Append(v1); // добавление вертекса
+		VertexOutput v2 = { input[1].color, { 0, 0 }, input[1].position };
+		stream.Append(v2); // добавление вертекса
+		VertexOutput v3 = { input[2].color, { 0, 0 }, input[2].position };
+		stream.Append(v3); // добавление вертекса
     
     
-    for (uint i = 0; i < 3; i++)
-    {
-		float4 p1 = input[i].position;
-		float4 p2 = input[(i + 1) % 3].position;
-		p1 /= p1.w;
-		p2 /= p2.w;
+		for (uint i = 0; i < 3; i++)
+		{
+			float4 p1 = input[i].position;
+			float4 p2 = input[(i + 1) % 3].position;
+			p1 /= p1.w;
+			p2 /= p2.w;
         
         //u and v are perpendiculat to each other and tubeDir
-		float3 tubeDir = normalize(p1.xyz - p2.xyz);
-		float u;
-		if (tubeDir != float3(0, 0, 1))
-			u = normalize(cross(tubeDir, float3(0, 0, 1)));
-        else
-			u = normalize(cross(tubeDir, float3(1, 1, 0)));
-		float v = cross(tubeDir, v);
+			float3 tubeDir = normalize(p1.xyz - p2.xyz);
+			float3 u;
+			if (tubeDir.x != 0 && tubeDir.y != 0)
+				u = normalize(cross(tubeDir, float3(0, 0, 1)));
+			else
+				u = normalize(cross(tubeDir, float3(1, 0, 0)));
+			float3 v = cross(tubeDir, v);
 
+		}
+
+		stream.RestartStrip(); // создаем примитив 
 	}
 
-        stream.RestartStrip(); // создаем примитив 
-}
