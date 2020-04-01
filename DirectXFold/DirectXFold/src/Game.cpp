bae474378762994cc72d9bb891789edcbc3f5442
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-01 14:00:26 +0700

    резня

--- DirectXFold/DirectXFold/src/Game.cpp
+++ DirectXFold/DirectXFold/src/Game.cpp
@@ -574,11 +574,10 @@ bool Game::LoadContent()
     //m_camera->Move(SphericalRotationZW(XM_PI-0.3f));
 
     {
-        //todo: странное управление FC_Z. Исправить
-        XMFLOAT4 arr[] = { XMFLOAT4(1.f, 0.f, 0.f, 1.f), XMFLOAT4(0.f, 1.f, 0.f, 1.f), XMFLOAT4(0.f, 0.f, 1.f, 1.f), XMFLOAT4(1.f, 1.f, 0.f, 1.f), XMFLOAT4(1.f, 0.f, 1.f, 1.f), XMFLOAT4(0.f, 1.f, 1.f, 1.f) };
         //mesh1 = new SphericalOctahedron(SphericalOctahedron::FixedCoordinate::FC_W, .99f, SphericalRotationXZ(XM_PIDIV4), arr);
 
-        mesh1 = new SphericalOctahedron(.99f, SphericalRotationXZ(XM_PIDIV4), arr);
+        XMFLOAT4 arrOct[] = { XMFLOAT4(1.f, 0.f, 0.f, 1.f), XMFLOAT4(0.f, 1.f, 0.f, 1.f), XMFLOAT4(0.f, 0.f, 1.f, 1.f), XMFLOAT4(1.f, 1.f, 0.f, 1.f), XMFLOAT4(1.f, 0.f, 1.f, 1.f), XMFLOAT4(0.f, 1.f, 1.f, 1.f) };
+        mesh1 = new SphericalOctahedron(.99f, SphericalRotationXZ(XM_PIDIV4), arrOct);
         meshes.push_back(mesh1);
 
         SphericalCube(0.9f);
@@ -588,30 +587,6 @@ bool Game::LoadContent()
 
         Mesh* test = SphericalMeshLoader::LoadMesh("mesh1.sph");
         //meshes.push_back(test);
-        /*float v1 = 0.99f, v3 = 0.08f, v2 = sqrtf(1.f - v1 * v1 - v3 * v3);
-        Mesh::VertexPosColor vertices[] = {
-            { XMFLOAT4(v2, 0.f, v3, v1), XMFLOAT4(1.f, 0.f, 1.f, 1.0f) },   // 0
-            { XMFLOAT4(0.f,  v2, -v3, v1), XMFLOAT4(0.f, 1.f, 1.f, 1.0f) },  // 1
-            { XMFLOAT4(0.f,  -v2, -v3, v1), XMFLOAT4(1.f, 1.f, 0.f, 1.0f) }, // 2
-            { XMFLOAT4(-v2, 0.f, v3, v1), XMFLOAT4(0.f, 0.f, 1.f, 1.0f) }   // 3
-        };
-
-        WORD indices[] = {
-            2, 1, 0,
-            1, 2, 3,
-            0, 1, 3,
-            3, 2, 0
-        };*/
-
-        /*Mesh::VertexPosColor vertices[] = {
-            { XMFLOAT4(0.6f, 0.0f, 0.0f, 0.8f), XMFLOAT4(0.0f, 0.0f, 0.0f, 1.f) },   // 0
-            { XMFLOAT4(0.0f,  0.6f, 0.0f, 0.8f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.f) },  // 1
-            { XMFLOAT4(0.0f,  -0.6f, 0.0f, 0.8f), XMFLOAT4(1.0f, 1.0f, 0.0f, 1.f) }, // 2
-        };
-
-        WORD indices[] = {
-            0, 1, 2, 2, 1, 0
-        };*/
 
         float v1 = 0.99f, v3 = 0.08f, v2 = sqrtf(1.f - v1 * v1 - v3 * v3);
         Mesh::VertexPosColor vertices[] = {
