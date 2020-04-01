48ddf9734a2e7983e0e171bea945819e457bce37
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-01 13:58:43 +0700

    куб добавлен надо тестить

--- DirectXFold/DirectXFold/src/SphericalCube.cpp
+++ DirectXFold/DirectXFold/src/SphericalCube.cpp
@@ -54,9 +54,58 @@ SphericalCube::SphericalCube(double wSec, XMMATRIX world, XMFLOAT4* colors)
             0, 2, 4,
             4, 2, 6,
 
-            0, 2, 1,
+            0, 1, 2,
             3, 2, 1,
 
+            4, 6, 7,
+            5, 4, 7,
 
+            2, 3, 7,
+            2, 7, 6,
+
+            1, 4, 0,
+            1, 4, 5,
+
+            5, 7, 3,
+            3, 1, 5
     };
+
+
+    auto& game = Game::GetInstance();
+    auto device = game.g_d3dDevice;
+    deviceContext = game.g_d3dDeviceContext;
+    d3dConstantBuffer = game.g_d3dVSConstantBuffers[2];
+    g_Indices = indices;
+
+    indicesCount = _countof(indices);
+
+    // Create and initialize the vertex buffer.
+    D3D11_BUFFER_DESC vertexBufferDesc;
+    ZeroMemory(&vertexBufferDesc, sizeof(D3D11_BUFFER_DESC));
+
+    vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;  //how the buffer is bound to pipeline
+    vertexBufferDesc.ByteWidth = sizeof(VertexPosColor) * verticesCount;
+    vertexBufferDesc.CPUAccessFlags = 0;    // no CPU access is necessary
+    vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+
+    //used to specify the data that is used to initialize a buffer when it is created.
+    D3D11_SUBRESOURCE_DATA resourceData;
+    ZeroMemory(&resourceData, sizeof(D3D11_SUBRESOURCE_DATA));
+    resourceData.pSysMem = g_Vertices; //A pointer to the data to initialize the buffer with.
+
+    device->CreateBuffer(&vertexBufferDesc, &resourceData, &g_d3dVertexBuffer);
+
+    // Create and initialize the index buffer.
+    D3D11_BUFFER_DESC indexBufferDesc;
+    ZeroMemory(&indexBufferDesc, sizeof(D3D11_BUFFER_DESC));
+
+    indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    indexBufferDesc.ByteWidth = sizeof(WORD) * indicesCount;
+    indexBufferDesc.CPUAccessFlags = 0;
+    indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+    resourceData.pSysMem = g_Indices;
+
+    device->CreateBuffer(&indexBufferDesc, &resourceData, &g_d3dIndexBuffer);
+
+    constantBuffer.m_world = world;
 }
