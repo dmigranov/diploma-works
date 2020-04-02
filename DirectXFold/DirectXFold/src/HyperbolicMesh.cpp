9057bf163d8a8e63e6a217d88150456eaa87bed6
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-02 20:44:28 +0700

    хватит

--- DirectXFold/DirectXFold/src/HyperbolicMesh.cpp
+++ DirectXFold/DirectXFold/src/HyperbolicMesh.cpp
@@ -13,6 +13,27 @@ HyperbolicMesh::HyperbolicMesh(int nv, VertexPosColor* vertices, int ni, WORD* i
 {
 }
 
+void HyperbolicMesh::Render(std::list<DirectX::XMMATRIX> matrices)
+{
+    // Input Assembler Stage - unique for every mesh
+    const UINT vertexStride = sizeof(VertexPosColor);   //Each stride is the size (in bytes) of the elements that are to be used from that vertex buffer.
+    const UINT offset = 0;
+    deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    deviceContext->IASetVertexBuffers(0, 1, &g_d3dVertexBuffer, &vertexStride, &offset);
+    deviceContext->IASetIndexBuffer(g_d3dIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+
+    for (auto matrix : matrices)
+    {
+        MeshConstantBuffer constantBufferTemp = { matrix };
+        if (parentMesh != nullptr)
+            constantBufferTemp.m_world = constantBufferTemp.m_world * parentMesh->GetWorldMatrix();
+        deviceContext->UpdateSubresource(d3dConstantBuffer, 0, nullptr, &constantBufferTemp, 0, 0);
+
+        //DRAW
+        deviceContext->DrawIndexed(indicesCount, 0, 0);
+    }
+}
+
 void HyperbolicMesh::Render()
 {
     // Input Assembler Stage - unique for every mesh
@@ -23,6 +44,24 @@ void HyperbolicMesh::Render()
     deviceContext->IASetIndexBuffer(g_d3dIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
 
     MeshConstantBuffer constantBufferTemp = { constantBuffer.m_world };
+    if (parentMesh != nullptr)
+        constantBufferTemp.m_world = constantBufferTemp.m_world * parentMesh->GetWorldMatrix();
+    deviceContext->UpdateSubresource(d3dConstantBuffer, 0, nullptr, &constantBufferTemp, 0, 0);
+
+    //DRAW
+    deviceContext->DrawIndexed(indicesCount, 0, 0);
+}
+
+void HyperbolicMesh::Render(DirectX::XMMATRIX matrix)
+{
+    // Input Assembler Stage - unique for every mesh
+    const UINT vertexStride = sizeof(VertexPosColor);   //Each stride is the size (in bytes) of the elements that are to be used from that vertex buffer.
+    const UINT offset = 0;
+    deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    deviceContext->IASetVertexBuffers(0, 1, &g_d3dVertexBuffer, &vertexStride, &offset);
+    deviceContext->IASetIndexBuffer(g_d3dIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+
+    MeshConstantBuffer constantBufferTemp = { matrix };
     if (parentMesh != nullptr)
         constantBufferTemp.m_world = constantBufferTemp.m_world * parentMesh->GetWorldMatrix();
     deviceContext->UpdateSubresource(d3dConstantBuffer, 0, nullptr, &constantBufferTemp, 0, 0);
