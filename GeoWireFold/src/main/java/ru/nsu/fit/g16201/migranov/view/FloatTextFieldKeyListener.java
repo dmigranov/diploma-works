0bf9ef24c2e78d342a1309f894d22b6e3695c1b5
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-04 18:33:11 +0700

    хорошечно

--- GeoWireFold/src/main/java/ru/nsu/fit/g16201/migranov/view/FloatTextFieldKeyListener.java
+++ GeoWireFold/src/main/java/ru/nsu/fit/g16201/migranov/view/FloatTextFieldKeyListener.java
@@ -8,7 +8,7 @@ public class FloatTextFieldKeyListener extends KeyAdapter {
     public void keyTyped(KeyEvent e) {
         super.keyTyped(e);
         char c = e.getKeyChar();
-        if(!((c >= '0' && c <= '9') || c == KeyEvent.VK_BACK_SPACE || c == KeyEvent.VK_DELETE || c == '.'))
+        if(!((c >= '0' && c <= '9') || c == KeyEvent.VK_BACK_SPACE || c == KeyEvent.VK_DELETE || c == '.' || c == '-'))
         {
             e.consume();
         }
