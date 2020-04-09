cbe3f333f8bbda893cc76ce6eec95640113d59aa
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-09 17:02:22 +0700

    переключение работает

--- DirectXFold/DirectXFold/VertexEllipticalExpSqFogShader.hlsl
+++ DirectXFold/DirectXFold/VertexEllipticalExpSqFogShader.hlsl
@@ -1,5 +1,66 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+cbuffer PerApplication : register(b0)
 {
-	//
-	return pos;
+	matrix projectionMatrixFront;
+	matrix projectionMatrixBack;
+	float density;
+}
+
+cbuffer PerFrame : register(b1)
+{
+	matrix viewMatrixFront;
+	matrix viewMatrixBack;
+}
+
+cbuffer PerObject : register(b2)
+{
+	matrix worldMatrix;
+}
+static float PI = 3.14159265;
+
+//POSITION and COLOR are semantics that are used to link vs variables to ps variables
+struct VertexShaderInput
+{
+	float4 position : POSITION;
+	float4 color : COLOR;
+};
+
+struct VertexShaderOutput
+{
+	float4 position : SV_POSITION;
+	float4 color : COLOR;
+	float fogFactor : FOG_FACTOR;
+};
+
+//entry point
+VertexShaderOutput main(VertexShaderInput IN, uint instanceID : SV_InstanceID)
+{
+	VertexShaderOutput OUT;
+ 
+	matrix viewMatrix, projectionMatrix;
+
+
+	if (instanceID % 2 == 0)
+	{
+		projectionMatrix = projectionMatrixFront;
+		viewMatrix = viewMatrixFront;
+	}
+	else if (instanceID % 2 == 1)
+	{
+		projectionMatrix = projectionMatrixBack;
+		viewMatrix = viewMatrixBack;
+	}
+
+	matrix viewWorld;
+	viewWorld = mul(viewMatrix, worldMatrix);
+	
+	float4 cameraSpacePosition = mul(viewWorld, IN.position);
+	float chordLength = distance(float4(0, 0, 0, 1), cameraSpacePosition); //длина хорды
+	float distance = 2 * asin(chordLength / 2.);
+	
+	OUT.color = IN.color;
+	OUT.position = mul(projectionMatrix, cameraSpacePosition);
+	
+	OUT.fogFactor = saturate(exp(-pow(density * distance, 2)));
+	
+	return OUT;
 }
