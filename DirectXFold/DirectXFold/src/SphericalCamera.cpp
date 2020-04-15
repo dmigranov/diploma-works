735aa220e9374b0ace074e26fd11e4750bd8a51a
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-15 18:35:39 +0700

    й

--- DirectXFold/DirectXFold/src/SphericalCamera.cpp
+++ DirectXFold/DirectXFold/src/SphericalCamera.cpp
@@ -42,12 +42,8 @@ const XMMATRIX& SphericalCamera::GetView()
 		//T = T * SphericalRotationZW(m_position.z) * SphericalRotationYW(m_position.y) * SphericalRotationXW(m_position.x) ;		T = T * R * SphericalRotationXW(m_position.x) * SphericalRotationYW(m_position.y) * SphericalRotationZW(m_position.z) * R.Invert();
 
 		T = T * R * SphericalRotationXW(m_position.x) * SphericalRotationYW(m_position.y) * SphericalRotationZW(m_position.z) * R.Invert();
-		//m_view = T * R ;
+		m_view = T * R ;
 
-		T = SphericalRotationZW(-m_position.z) * SphericalRotationYW(-m_position.y) * SphericalRotationXW(-m_position.x);
-		R = SphericalRotationXZ(-yawDelta) * SphericalRotationYZ(-pitchDelta);
-		cameraTransform = R * T * cameraTransform;
-		m_view = cameraTransform.Invert();
 
 		/*T = T * R * SphericalRotationXW(m_position.x) * SphericalRotationYW(m_position.y) * SphericalRotationZW(m_position.z);
 		m_view = T;*/
