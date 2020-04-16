f2bb8edca2ede6efc8c47c0ac5f673130ee88c3b
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-16 19:07:49 +0700

    создан класс дял текстуры

---
+++ DirectXFold/DirectXFold/inc/Texture.h
@@ -0,0 +1,17 @@
+#pragma once
+class Texture
+{
+public:
+	Texture();
+	Texture(const Texture&);
+	~Texture();
+
+	bool Initialize(ID3D11Device*, WCHAR*);
+	void Shutdown();
+
+	ID3D11ShaderResourceView* GetTexture();
+
+private:
+	ID3D11ShaderResourceView* m_texture;
+};
+
---
+++ DirectXFold/DirectXFold/src/Texture.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "Texture.h"
+
+Texture::Texture()
+{
+	m_texture = nullptr;
+}
