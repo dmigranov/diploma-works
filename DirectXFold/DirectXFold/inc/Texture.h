6c58f5aa164d6d8b5dcf77b3fe203153bb0840b4
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-16 19:24:28 +0700

    класс текстуры

--- DirectXFold/DirectXFold/inc/Texture.h
+++ DirectXFold/DirectXFold/inc/Texture.h
@@ -6,12 +6,14 @@ public:
 	Texture(const Texture&);
 	~Texture();
 
-	bool Initialize(ID3D11Device*, WCHAR*);
+	bool Initialize(ID3D11Device* device, WCHAR* name);
 	void Shutdown();
 
 	ID3D11ShaderResourceView* GetTexture();
 
 private:
 	ID3D11ShaderResourceView* m_texture;
+	ID3D11Resource* m_resource;
+
 };
 
--- DirectXFold/DirectXFold/src/Texture.cpp
+++ DirectXFold/DirectXFold/src/Texture.cpp
@@ -1,7 +1,51 @@
 #include "pch.h"
 #include "Texture.h"
+#include "DDSTextureLoader.h"
+
 
 Texture::Texture()
 {
 	m_texture = nullptr;
+}
+
+Texture::Texture(const Texture&)
+{
+}
+
+Texture::~Texture()
+{
+}
+
+bool Texture::Initialize(ID3D11Device* device, WCHAR* name)
+{
+
+	HRESULT result = DirectX::CreateDDSTextureFromFile(device, name, &m_resource, &m_texture); 
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void Texture::Shutdown()
+{
+	if (m_texture)
+	{
+		m_texture->Release();
+		m_texture = nullptr;
+	}
+
+	if (m_resource)
+	{
+		m_resource->Release();
+		m_resource = nullptr;
+	}
+
+	return;
+}
+
+ID3D11ShaderResourceView* Texture::GetTexture()
+{
+	return m_texture;
 }
