c035472d57ff79f0589b02e3f068303e6f004103
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-18 21:39:24 +0700

    изменено название проги

--- DirectXFold/DirectXFold/src/main.cpp
+++ DirectXFold/DirectXFold/src/main.cpp
@@ -62,7 +62,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
         AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
 
-        HWND hwnd = CreateWindowExW(0, L"Direct3DGameWindowClass", L"Direct3DGame", WS_OVERLAPPEDWINDOW,
+        HWND hwnd = CreateWindowExW(0, L"Direct3DGameWindowClass", L"Spherical & Elliptical Space", WS_OVERLAPPEDWINDOW,
             CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
             nullptr);
         // TODO: Change to CreateWindowExW(WS_EX_TOPMOST, L"Direct3DGameWindowClass", L"Direct3DGame", WS_POPUP,
