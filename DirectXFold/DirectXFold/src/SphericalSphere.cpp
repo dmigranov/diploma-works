73342dde47a6587af3db016c0f68e68100953790
Denis Migranov <d.migranov@g.nsu.ru>
2020-04-26 18:07:31 +0700

    прогресс

--- DirectXFold/DirectXFold/src/SphericalSphere.cpp
+++ DirectXFold/DirectXFold/src/SphericalSphere.cpp
@@ -15,4 +15,24 @@ SphericalSphere::SphericalSphere(float radius, int sliceCount, int stackCount, D
 
 	vertices.push_back({ XMFLOAT4(0.f, radius, 0.f, height), color, XMFLOAT2(0.f ,0.f) });
 
+    for (int i = 1; i <= stackCount - 1; i++) {
+        auto phi = i * phiStep;
+        for (int j = 0; j <= sliceCount; j++) {
+            auto theta = j * thetaStep;
+            XMFLOAT4 pos(
+                (radius * sinf(phi) * cosf(theta)),
+                (radius * cosf(phi)),
+                (radius * sinf(phi) * sinf(theta)),
+                height
+                )
+            ;
+
+            var t = new Vector3(-radius * MathF.Sin(phi) * MathF.Sin(theta), 0, radius * MathF.Sin(phi) * MathF.Cos(theta));
+            t.Normalize();
+            var n = p;
+            n.Normalize();
+            var uv = new Vector2(theta / (MathF.PI * 2), phi / MathF.PI);
+            ret.Vertices.Add(new Vertex(p, n, t, uv));
+        }
+    }
 }
