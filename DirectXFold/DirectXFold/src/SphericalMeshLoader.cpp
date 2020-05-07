1a543d054144c77e0230d36945fff780eed3090f
Denis Migranov <d.migranov@g.nsu.ru>
2020-05-07 14:33:21 +0700

    начато исправление загрузки меша из файла

--- DirectXFold/DirectXFold/src/SphericalMeshLoader.cpp
+++ DirectXFold/DirectXFold/src/SphericalMeshLoader.cpp
@@ -19,11 +19,48 @@ SphericalMesh* SphericalMeshLoader::LoadMesh(const char* filepath)
 
 	std::vector<Mesh::VertexPosColor> vertices;
 	std::vector<WORD> vertexIndices;
+	
+	std::vector<Vector3> positions;
+	std::vector<int> positionIndices;
+	std::vector<Vector2> uv0;
+	std::vector<int> textureIndices;
 
 
 	while (std::getline(infile, str))
 	{
-		if (str.length() > 1 && str[0] == 'v' && str[1] == ' ') //v -0.5 0.5 -0.5
+		if (str[0] == 'v')
+		{
+			if (str[1] == ' ') //v -0.5 0.5 -0.5 1
+			{
+				str = str.substr(2);
+				std::vector<std::string> parsedStrings = parseString(str, ' ');
+				std::vector<double> values = getDoubleValues(parsedStrings);
+				positions.push_back(Vector3(values[0], values[1], values[2]));
+			}
+			else if (str[1] == 't') //vt 0 0
+			{
+				str = str.substr(3);
+				std::vector<std::string> parsedStrings = parseString(str, ' ');
+				std::vector<double> values = getDoubleValues(parsedStrings);
+				uv0.push_back(Vector2(values[0], values[1]));
+			}
+		}
+		else if (str[0] == 'f') //f 2/2 4/4 1/1
+		{
+			str = str.substr(2);
+			std::vector<std::string> parsedStrings = parseString(str, ' ');
+			for (std::string substr : parsedStrings) //2/2
+			{
+				std::vector<std::string> strIndices = parseString(substr, '/');
+				std::vector<int> indices = getIntValues(strIndices);
+
+				positionIndices.push_back(indices[0] - 1); //индексы считаются с единицы, а не с нуля
+				textureIndices.push_back(indices[1] - 1);
+			}
+		}
+
+
+		/*if (str.length() > 1 && str[0] == 'v' && str[1] == ' ') //v -0.5 0.5 -0.5
 		{
 
 			str = str.substr(2);
@@ -39,8 +76,10 @@ SphericalMesh* SphericalMeshLoader::LoadMesh(const char* filepath)
 			std::vector<std::string> strIndices = parseString(str, ' ');
 			std::vector<int> triangleIndices = getIntValues(strIndices);			
 			vertexIndices.insert(std::end(vertexIndices), std::begin(triangleIndices), std::end(triangleIndices));
-		}
+		}*/
 	}
+
+	//todo form Vertices and VertexIndices
 
 	return new SphericalMesh(vertices.size(), &vertices[0], vertexIndices.size(), &vertexIndices[0]);
 }
