efe9bbe7cf32436130cc54c965eecabf2d736b89
Denis Migranov <d.migranov@g.nsu.ru>
2020-05-20 13:49:08 +0700

    красиво короче пойдет

--- DirectXFold/DirectXFold/src/main.cpp
+++ DirectXFold/DirectXFold/src/main.cpp
@@ -16,41 +16,42 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _
     Game& game = Game::GetInstance();
     game.InitializeWindow(hInstance, nCmdShow);
 
-
-    auto earthTexture = game.CreateTexture(L"earth.dds");
-    auto asteroidTexture = game.CreateTexture(L"asteroid2.dds");
-    auto fabricTexture = game.CreateTexture(L"fabric.dds");
-
-    game.MoveCamera(Vector3(0, 0, -XM_PI / 4));
-    game.SetCameraFovY(XM_PI / 2);
-    auto mesh1 = new SphericalSphere(0.15f, 20, 20, earthTexture);
-    mesh1->AddUpdater(SphericalMesh::MeshUpdater([](Matrix in, float delta) {
-        auto ks = Keyboard::Get().GetState();
-
-        float gain = 0.045f;
-        Matrix m = Matrix::Identity;
-        if (ks.U)
-            m = SphericalRotationYW(-gain); //так всегда вверх!
-        if (ks.J)
-            m = SphericalRotationYW(gain);
-        if (ks.H)
-            m = SphericalRotationXW(gain);
-        if (ks.K)
-            m = SphericalRotationXW(-gain);
-
-        return in * m;
-    }));
-    game.AddMesh(mesh1);
-
-
-    int sect = 9;
-    for (int i = 1; i < sect; i++)
     {
-        Mesh* mesh = new SphericalSphere(0.15f, 20, 20, earthTexture, SphericalRotationZW(i * XM_PI / sect));
-        /*mesh->AddUpdater(SphericalMesh::MeshUpdater([i](Matrix in, float delta) {
-            return SphericalRotationYZ(delta / i / 3.f) * SphericalRotationXY(delta / i / 2.f) * in * SphericalRotationYW(-delta / i / 6.f) * SphericalRotationZW(delta / 3.f) * SphericalRotationXW(delta / 12.f);
-        }));*/
-        game.AddMesh(mesh);
+        auto earthTexture = game.CreateTexture(L"earth.dds");
+        auto asteroidTexture = game.CreateTexture(L"asteroid2.dds");
+        auto fabricTexture = game.CreateTexture(L"fabric.dds");
+
+        game.MoveCamera(Vector3(0, 0, -XM_PI / 4));
+        game.SetCameraFovY(XM_PI / 2);
+        auto mesh1 = new SphericalSphere(0.15f, 20, 20, earthTexture);
+        mesh1->AddUpdater(SphericalMesh::MeshUpdater([](Matrix in, float delta) {
+            auto ks = Keyboard::Get().GetState();
+
+            float gain = 0.045f;
+            Matrix m = Matrix::Identity;
+            if (ks.U)
+                m = SphericalRotationYW(-gain); //так всегда вверх!
+            if (ks.J)
+                m = SphericalRotationYW(gain);
+            if (ks.H)
+                m = SphericalRotationXW(gain);
+            if (ks.K)
+                m = SphericalRotationXW(-gain);
+
+            return in * m;
+        }));
+        game.AddMesh(mesh1);
+
+
+        int sect = 9;
+        for (int i = 1; i < sect; i++)
+        {
+            Mesh* mesh = new SphericalSphere(0.15f, 20, 20, earthTexture, SphericalRotationZW(i * XM_PI / sect));
+            /*mesh->AddUpdater(SphericalMesh::MeshUpdater([i](Matrix in, float delta) {
+                return SphericalRotationYZ(delta / i / 3.f) * SphericalRotationXY(delta / i / 2.f) * in * SphericalRotationYW(-delta / i / 6.f) * SphericalRotationZW(delta / 3.f) * SphericalRotationXW(delta / 12.f);
+            }));*/
+            game.AddMesh(mesh);
+        }
     }
 
     return game.StartGame(hInstance, nCmdShow);
