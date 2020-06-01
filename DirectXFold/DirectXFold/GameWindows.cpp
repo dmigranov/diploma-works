f8784158f631a45f6d1172bae70c98b1e3a19db5
Denis Migranov <d.migranov@g.nsu.ru>
2020-06-01 16:22:09 +0700

    ss

--- DirectXFold/DirectXFold/GameWindows.cpp
+++ DirectXFold/DirectXFold/GameWindows.cpp
@@ -83,7 +83,7 @@ int Game::InitializeEngine(HINSTANCE hInstance, int nCmdShow, boolean isConsoleE
 
         AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
 
-        HWND hwnd = CreateWindowExW(0, L"Direct3DGameWindowClass", L"Spherical & Elliptical Space", WS_OVERLAPPEDWINDOW,
+        HWND hwnd = CreateWindowExW(0, L"Direct3DGameWindowClass", L"Spherical & Elliptical Spaces Visualizer", WS_OVERLAPPEDWINDOW,
             CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
             nullptr);
         // TODO: Change to CreateWindowExW(WS_EX_TOPMOST, L"Direct3DGameWindowClass", L"Direct3DGame", WS_POPUP,
